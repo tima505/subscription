b2a7bb1026052e18c97ae7591fadeef1413377ec
tima505 <t55885445@gmail.com>
2026-05-05 21:49:56 +0500

    Exclude static paths from catch-all route

--- backend/urls.py
+++ backend/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!assets/)(?!static/).*$', TemplateView.as_view(template_name='index.html')),
 ]
